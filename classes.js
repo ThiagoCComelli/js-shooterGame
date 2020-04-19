cb3855810d30afbb9695ead814c9beac7e672800
ThiagoCComelli <thiago.comelli@outlook.com>
2020-04-18 22:11:19 -0300

    update

--- classes.js
+++ classes.js
@@ -1,9 +1,16 @@
+
 class Player{
-    constructor(){
+    constructor(nome){
+        this.nome = nome
         this.x = canv.width/2
         this.y = canv.height/2
         this.r = 20
+        this.life = 100
+        this.money = 0
+        this.kills = 0
+        this.level = 0
         this.weapon = new Weapon()
+        this.healthbar = null
     }
 
     get getPos(){
@@ -12,6 +19,10 @@ class Player{
 
     get getCooldown(){
         return this.cooldown
+    }
+
+    addMoney(money_){
+        this.money += money_
     }
 
     move(valX,valY){
@@ -24,6 +35,9 @@ class Player{
         this.y = this.y < 0 ? this.y = 0 : this.y
         this.y = this.y > canv.height ? this.y = canv.height : this.y
         
+        if(this.healthbar != null){
+            this.healthbar.move(valX,valY)
+        }
     }
 
     shoot(){
@@ -37,6 +51,10 @@ class Player{
         ctx.fill()
 
         this.weapon.draw()
+
+        if(this.healthbar != null){
+            this.healthbar.draw()
+        }
     }
 }
 
@@ -46,7 +64,7 @@ class Weapon{
         this.width = 50
         this.height = 10
         this.cooldownWeapon = 0
-        this.cooldownWeaponReady = 10
+        this.cooldownWeaponReady = WEAPON_COOLDOWN
     }
 
     shoot(){
@@ -75,15 +93,25 @@ class Bullet{
         this.angleL = Math.atan2(MOUSEY-player.getPos.y,MOUSEX-player.getPos.x)
         this.translateX = player.getPos.x
         this.translateY = player.getPos.y
-        this.damage = 10
+        this.damage = BULLET_DAMAGE
     }
 
     get position(){
         return {x: this.x, y:this.y}
     }
 
-    get getDamage(){
-        return this.damage
+    get getStats(){
+        return this.r
+    }
+    
+    get bulletXY(){
+        let dx = canv.width-(canv.width-this.x)
+        let dy = canv.height-(canv.height-this.y)
+        let length = Math.sqrt(dx*dx+dy*dy)
+        let bulletAngle = Math.atan2(dy,dx)
+        let screenX = this.translateX+length*Math.cos(bulletAngle+this.angleL)
+        let screenY = this.translateY+length*Math.sin(bulletAngle+this.angleL)
+        return {x:screenX,y:screenY}
     }
 
     draw(){
@@ -102,18 +130,86 @@ class Bullet{
     }
 }
 
-// class Enemy{
-//     constructor(){
-//         this.x = Math.floor(Math.random() * canv.width) + 0 
-//         this.y = Math.floor(Math.random() * canv.height) + 0
-//         this.r = 20
-//         this.translateX = canv.width/2
-//         this.translateY = canv.height/2
-//     }
+class Enemy{
+    constructor(life){
+        this.life = life
+        this.x = Math.floor(Math.random() * canv.width) + 0 
+        this.y = Math.floor(Math.random() * canv.height) + 0
+        this.r = 20
+        this.damage = 1.5
+        this.translateX = canv.width/2
+        this.translateY = canv.height/2
+        this.healthbar = new HealthBar(this.x-(this.r*1.25),this.y+this.r+5,false)
+    }
 
-//     draw(){
-//         ctx.arc(this.x,this.y,this.r,0,Math.PI*2)
-//         ctx.fillStyle = "black"
-//         ctx.fill()
-//     }
-// }
+    checkCollisionBullet(bulletX,bulletY,bullet_,enemy_){
+        if((Math.sqrt(Math.pow(bulletX-this.x,2) + Math.pow(bulletY-this.y,2))) <= this.r+bullet_.r){
+            this.life -= bullet_.damage
+            if(this.life <= 0){
+                player.addMoney(Math.random() * 0.7 + 0.3)
+                player.kills++
+                return {hit:true,kill:true}
+            }
+            let size = (enemy_.healthbar.sizeBar.w*this.life-bullet_.damage)/ENEMY_LIFE
+            enemy_.healthbar.sizeLifeBar.w = size
+
+            return {hit:true,kill:false}
+        } else {
+            return {hit:false,kill:false}
+        }
+    }
+
+    checkCollisionPlayer(enemy_){
+        if((Math.sqrt(Math.pow(this.x-player.x,2) + Math.pow(this.y-player.y,2))) <= this.r*2){
+            if(player.healthbar == null){
+                player.healthbar = new HealthBar(player.x-(player.r*1.25),player.y+player.r+5,true)
+            } 
+            player.life -= this.damage
+            let size = (player.healthbar.sizeBar.w*player.life-enemy_.damage)/PLAYER_LIFE
+            player.healthbar.sizeLifeBar.w = size
+
+            return {hit:true}
+        } else {
+            return {hit:false}
+        }
+        
+    }
+
+    draw(){
+        ctx.beginPath()
+        ctx.arc(this.x,this.y,this.r,0,Math.PI*2)
+        ctx.fillStyle = "black"
+        ctx.fill()
+
+        if(this.life < ENEMY_LIFE){
+            this.healthbar.draw()
+        }
+    }
+}
+
+class HealthBar{
+    constructor(x,y,player){
+        this.position = {x:x,y:y}
+        this.sizeBar = {w:50,h:10}
+        this.sizeLifeBar = {w:50,h:10}
+        this.playerVerify = player
+    }
+
+    move(x,y){
+        if(this.playerVerify){
+            this.position.x += x
+            this.position.y += y
+        }
+    }
+
+    draw(){
+        ctx.beginPath()
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.position.x,this.position.y,this.sizeBar.w,this.sizeBar.h)
+        ctx.fill()
+        ctx.beginPath()
+        ctx.fillStyle = "green"
+        ctx.fillRect(this.position.x,this.position.y,this.sizeLifeBar.w,this.sizeLifeBar.h)
+        ctx.fill()
+    }
+}
--- script.js
+++ script.js
@@ -17,58 +17,161 @@ document.addEventListener("mouseup",function(e){
 
 var canv = document.getElementById("canvasGame")
 var ctx = canv.getContext("2d")
+var canvStats = document.getElementById("canvasStats")
+var ctxStats = canvStats.getContext("2d")
+var canvShop = document.getElementById("canvasShop")
+var ctxShop = canvShop.getContext("2d")
 var keys = {}
 var bullets = []
-var player
+var enemys = []
+var player = new Player("Thiago Comelli")
 var enemy
 var MOUSEX
 var MOUSEY
 var angle
-var FPS = 60
-var VELOCITY = 5
-var BULLET_SPEED = 10
+var FPS
+var VELOCITY
+var BULLET_SPEED
+var BULLET_DAMAGE
+var WEAPON_COOLDOWN
+var ENEMY_LIFE
+var PLAYER_LIFE
+var gameState
 
 
 function setup(){
-    player = new Player()
-    enemy = new Enemy()
+    canv = document.getElementById("canvasGame")
+    ctx = canv.getContext("2d")
+    canvStats = document.getElementById("canvasStats")
+    ctxStats = canvStats.getContext("2d")
+    canvShop = document.getElementById("canvasShop")
+    ctxShop = canvShop.getContext("2d")
+    enemys = []
+    FPS = 60
+    VELOCITY = 5
+    BULLET_SPEED = 20
+    BULLET_DAMAGE = 10
+    WEAPON_COOLDOWN = 0
+    ENEMY_LIFE = 100
+    PLAYER_LIFE = 100
+    gameState = true
+
+    player = new Player("Thiago Comelli")
+
+    spawnEnemys()
+}
+
+function spawnEnemys(){
+    for(let i = 0;i<10;i++){
+        enemys.push(new Enemy(ENEMY_LIFE))
+    }
 }
 
 function move(){
-    if(37 in keys){
-        player.move(-VELOCITY,0)
-    } if (39 in keys){
-        player.move(VELOCITY,0)
-    } if (38 in keys){
-        player.move(0,-VELOCITY)
-    } if (40 in keys){
-        player.move(0,VELOCITY)
-    } if (3737 in keys){
-        player.shoot()
+    if(gameState == true){
+        if(37 in keys){
+            player.move(-VELOCITY,0)
+        } if (39 in keys){
+            player.move(VELOCITY,0)
+        } if (38 in keys){
+            player.move(0,-VELOCITY)
+        } if (40 in keys){
+            player.move(0,VELOCITY)
+        } if (3737 in keys){
+            player.shoot()
+        }
     }
 }
 
-function draw(){
-    angle = Math.atan2(MOUSEY-player.getPos.y,MOUSEX-player.getPos.x) 
+function drawShop(){
+    ctxShop.fillStyle = "rgb(175, 175, 175)"
+    ctxShop.fillRect(0,0,canvShop.width,canvShop.height)
 
-    ctx.fillStyle = "white"
+    ctxShop.fillStyle = "black"
+    ctxShop.font = "bold 30px Arial"
+    ctxShop.fillText("SHOP",canvShop.width/2-40,30)
+    ctxShop.font = "bold 17px Arial"
+    ctxShop.fillText("Damage:",10,88)
+    ctxShop.fillText("Player Speed:",10,130)
+    ctxShop.fillText("Bullet Speed:",10,170)
+    ctxShop.fillText("Cooldown:",10,210)
+
+    ctxShop.beginPath()
+    ctxShop.moveTo(0,240)
+    ctxShop.lineTo(200,240)
+    ctxShop.lineWidth = 2
+    ctxShop.stroke()
+
+}
+
+function drawStats(){
+    ctxStats.fillStyle = "rgb(175, 175, 175)"
+    ctxStats.fillRect(0,0,canvStats.width,canvStats.height)
+
+    ctxStats.fillStyle = "black"
+    ctxStats.font = "bold 30px Arial"
+    ctxStats.fillText("STATS",canvStats.width/2-40,30)
+    ctxStats.fillText("Name: "+player.nome,10,70)
+    ctxStats.fillText("Money: "+(player.money).toFixed(2),10,100)
+    ctxStats.fillText("Kill count: "+player.kills,10,130)
+    ctxStats.fillText("Bullet Speed: "+BULLET_SPEED,10,160)
+    ctxStats.fillText("Bullet Damage: "+BULLET_DAMAGE,10,190)
+    ctxStats.fillText("Player Life: "+player.life+"/100",canvStats.width/2,70)
+    ctxStats.fillText("Player Speed: "+VELOCITY,canvStats.width/2,100)
+    ctxStats.fillText("Player Level: "+player.level,canvStats.width/2,130)
+    ctxStats.fillText("Enemys alive: "+enemys.length,canvStats.width/2,160)
+    ctxStats.fillText("Weapon cooldown: "+player.weapon.cooldownWeaponReady,canvStats.width/2,190)
+}
+
+function drawGame(){
+    ctx.fillStyle = "rgb(150, 214, 108)"
     ctx.fillRect(0,0,canv.width,canv.height)
 
-    move()
-    
-    enemy.draw()
-    player.draw()
+    if (gameState == true){
+        angle = Math.atan2(MOUSEY-player.getPos.y,MOUSEX-player.getPos.x) 
 
-    for(let i = 0; i < bullets.length; i++){
-        bullets[i].draw()
-        // console.log(bullets[i].position.x)
-        let bullet = bullets[i]
-        if(bullet.position.x > canv.width || bullet.position.y > canv.height){
-            bullets.splice(i,1)
+        move()
+        
+        for(let i of enemys){
+            i.draw()
         }
-    }
+        player.draw()
 
+        for(let i = 0; i < bullets.length; i++){
+            bullets[i].draw()
+            let bullet = bullets[i]
+            if(bullet.bulletXY.x > canv.width || bullet.bulletXY.x < 0 || bullet.bulletXY.y > canv.height || bullet.bulletXY.y < 0){
+                bullets.splice(i,1)
+            } else {
+                for(let j = 0; j < enemys.length; j++){
+                    let condition = enemys[j].checkCollisionBullet(bullet.bulletXY.x,bullet.bulletXY.y,bullet,enemys[j])
+
+                    if (condition.hit) {
+                            bullets.splice(i,1)
+                            if(condition.kill){
+                                enemys.splice(j,1)
+                            }
+                        }
+                }
+            }
+        }
+        for(let i of enemys){
+            i.checkCollisionPlayer(i)
+        }
+        if(enemys.length == 0){
+            spawnEnemys()
+        }
+        if(player.life <= 0){
+            gameState = false
+        }
+        drawStats()
+        drawShop()
+    } else {
+        ctx.fillStyle = "black"
+        ctx.font = "100px Arial"
+        ctx.fillText("GAME OVER",100,canv.height/2)
+    }
 }
 
 setup()
-setInterval(draw,1000/FPS)
+setInterval(drawGame,1000/FPS)
